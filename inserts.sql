7015f4b4b73ef4fe95b366e2b3425588418521b6
flipperflopper <1016349@student.ccgs.wa.edu.au>
2024-08-07 11:09:30 +0800

    changes

--- inserts.sql
+++ inserts.sql
@@ -1,4 +1,4 @@
 INSERT INTO Menu (Name, GlutenOption, BasePrice) VALUES ("Garlic Bread Roll", FALSE, 6.00)
-INSERT INTO Menu (Name, GlutenOption, BasePrice) VALUES ("Artisan Garlic Bread (4 slices)", FALSE, 9.00)
+INSERT INTO Menu (Name, GlutenOption, BasePrice, Description) VALUES ("Artisan Garlic Bread (4 slices)", FALSE, 9.00, "Toasted sourdough with our famous home-made garlic butter")
 INSERT INTO Menu (Name, GlutenOption, BasePrice) VALUES ("Bruschetta (4 slices)", FALSE, 15.00)
 INSERT INTO Menu (Name, GlutenOption, BasePrice) VALUES ("Arancini", FALSE, 19.00)
